7b64c90b9de72bae2ebcb0c4939c3d6cbc784d67
yys7517 <yys7517@naver.com>
2026-05-13 12:11:57 +0900

    feat: User Entity 패키지 수정

---
+++ domain/src/main/java/com/example/domain/model/user/User.kt
@@ -0,0 +1,5 @@
+package com.example.domain.model.user
+
+data class User(
+    val id: Long
+)
--- domain/src/main/java/com/example/domain/model/user/request/User.kt
+++
@@ -1,5 +0,0 @@
-package com.example.domain.model.user.request
-
-data class User(
-    val id: Long
-)
