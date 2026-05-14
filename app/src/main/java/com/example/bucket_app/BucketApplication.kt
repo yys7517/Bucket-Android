004f07ed5398be176417801c14510977389b558c
yys7517 <yys7517@naver.com>
2026-05-14 09:48:34 +0900

    feat: 카카오 SDK init, Native Key 연결

--- app/src/main/java/com/example/bucket_app/BucketApplication.kt
+++ app/src/main/java/com/example/bucket_app/BucketApplication.kt
@@ -1,6 +1,7 @@
 package com.example.bucket_app
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 
@@ -11,5 +12,7 @@ class BucketApplication: Application() {
         if(BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
+
+        KakaoSdk.init(this, BuildConfig.KAKAO_NATIVE_KEY)
     }
 }
